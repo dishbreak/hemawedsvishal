d1931ba7c5767e01a4fd40a34ebed1fb9a361226
Vishal Kotcherlakota <vishal@kotcherlakota.org>
2016-08-16 21:00:10 -0400

    bare-bones web server

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+
+const PORT = 8080;
+
+var app = express();
+
+app.use("/", express.static(__dirname + "/public"));
+
+app.listen(PORT);
+
+console.log("listening on port 8080");
